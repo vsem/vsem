2ba346e11fed26eb9a59578d404bd1192c445c35
Elia Bruni <elia.bruni@gmail.com>
2013-12-04 15:43:50 +0100

    refact

--- toolbox/concepts/extractConcepts.m
+++ toolbox/concepts/extractConcepts.m
@@ -43,43 +43,46 @@ conceptSpace=[];
 conceptMatrixInitialized = false;
 % extracting concepts over the whole selected set of images
 for i = 1:size(imagePaths, 1)
-    
-    if opts.verbose
-        % handle for cancel button on progress bar
-        if ~waitBar.textualVersion && getappdata(waitBar.bar,'canceling')
-            break
+    conceptName = char(annotations{1}(1));
+    if ~isempty(strfind(conceptList,conceptName))
+        
+        if opts.verbose
+            % handle for cancel button on progress bar
+            if ~waitBar.textualVersion && getappdata(waitBar.bar,'canceling')
+                break
+            end
+            
+            % updating waitbar
+            waitBar.update(i);
         end
         
-        % updating waitbar
-        waitBar.update(i);
+        try
+            % extracting histogram and object list for the ith image
+            
+            [histogram, objectList] = extractConceptHistogram(encoder, ...
+                imagePaths{i}, ...
+                annotations{i}, ...
+                opts.conceptHistParams{:});
+            
+            if ~conceptMatrixInitialized
+                % initializing concept matrix with histogram dimension
+                conceptSpace.conceptMatrix = zeros(length(histogram), length(conceptList));
+                conceptSpace.conceptIndex = containers.Map(conceptList, 1:length(conceptList));
+                conceptMatrixInitialized = true;
+            end
+            
+            % updating concept space with the previously extracted data
+            conceptSpace = updateConceptMatrix(conceptSpace, histogram, objectList);
+            
+        catch ME
+            switch ME.identifier
+                case 'VSEM:FeatExt'
+                    fprintf(1, '%s\n', ME.message);
+                otherwise
+                    fprintf(1, 'Error reading file: %s\n', imagePaths{i});
+            end
+        end % try-catch block
     end
-    
-    try
-        % extracting histogram and object list for the ith image
-        
-        [histogram, objectList] = extractConceptHistogram(encoder, ...
-                                                          imagePaths{i}, ...
-                                                          annotations{i}, ...
-                                                          opts.conceptHistParams{:});
-        
-        if ~conceptMatrixInitialized
-            % initializing concept matrix with histogram dimension
-            conceptSpace.conceptMatrix = zeros(length(histogram), length(conceptList));
-            conceptSpace.conceptIndex = containers.Map(conceptList, 1:length(conceptList));
-            conceptMatrixInitialized = true;
-        end
-        
-        % updating concept space with the previously extracted data
-        conceptSpace = updateConceptMatrix(conceptSpace, histogram, objectList);
-        
-    catch ME
-        switch ME.identifier
-            case 'VSEM:FeatExt'
-                fprintf(1, '%s\n', ME.message);
-            otherwise
-                fprintf(1, 'Error reading file: %s\n', imagePaths{i});
-        end
-    end % try-catch block
 end % image iteration
 
 
@@ -87,10 +90,10 @@ end % image iteration
 function conceptSpace = updateConceptMatrix(conceptSpace, histogram, objectList, varargin)
 % -------------------------------------------------------------------------
 % update concept conceptSpace aggregator
-%   update(conceptSpace, histogram, objectList, varargin) aggregates one 
-%   or more histograms 'histogram' to a list of objects 'objectList' 
-%   of the same size, or aggregates one histogram to a list of 
-%   objects, regardless the size of the latter. It returns the updated 
+%   update(conceptSpace, histogram, objectList, varargin) aggregates one
+%   or more histograms 'histogram' to a list of objects 'objectList'
+%   of the same size, or aggregates one histogram to a list of
+%   objects, regardless the size of the latter. It returns the updated
 %   object.
 
 opts.aggregationFn = @sumFn;
