f80a29d0fd586741c54c1202d9caedb125e50ad6
Elia Bruni <elia.bruni@gmail.com>
2013-12-10 17:23:42 +0100

    refact

--- toolbox/concepts/utils/concat2Spaces.m
+++ toolbox/concepts/utils/concat2Spaces.m
@@ -3,7 +3,7 @@ function conceptSpace = concat2Spaces(conceptSpace1, conceptSpace2)
 % WARNING: that's ad hoc code to concat two spaces, but need
 % a lot of checking before making it official
 
-conceptList=[conceptSpace1.keys,conceptSpace2.keys];
+conceptList=[conceptSpace1.conceptIndex.keys,conceptSpace2.conceptIndex.keys];
 conceptSpace.conceptIndex = containers.Map(conceptList, 1:length(conceptList));
 
 conceptSpace.conceptMatrix=horzcat(conceptSpace1.conceptMatrix, conceptSpace2.conceptMatrix);
